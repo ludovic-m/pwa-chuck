63328abb5532281ca26f6fbd0655a376ed65308c
Ludovic Médard <ludovic.medard@live.com>
2017-12-07 21:34:21 +0100

    Delete previous version

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  // tslint:disable-next-line:max-line-length
-  factsApi: 'https://lmepwachucknorris.azurewebsites.net/api/GetLastFiveFacts?code=NXkN0leSRWVK80fgjqRJwdCiKTtxQ2NjPfVG5thTY9KwXNtz1MTuHA==',
-  config: {
-    'API_URL': 'http://localhost:7071',
-    'VAPID_PUB_KEY': 'BNOQmJn54JzYnFlbU8cgPXAOPXxd-eI0i0qobChcCW8AS9FdvmFkcIj9oA68QQoQVEPnR5mSiq8QTDzhmPBywSY'
-  }
-};
